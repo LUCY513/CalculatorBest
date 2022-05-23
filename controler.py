718cf2bd3d2ec84c0cf5caf8aff00a71bec10678
LUCY <Lucy@example.com>
2022-05-23 18:54:03 +0300

    Вычисление

---
+++ controler.py
@@ -0,0 +1,11 @@
+import using_interface as ui
+
+
+def result_work():
+
+    def arithmetic_work():
+        
+        value = eval(ui.get_value())
+        return value
+        
+    return ui.viwe_data(arithmetic_work())
